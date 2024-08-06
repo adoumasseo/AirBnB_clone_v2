ebe1a0cc51e93228acbe173fb816aad5b11e9c61
adoumasseo <adoumasseo@gmail.com>
2024-08-06 12:36:02 +0100

    Task 2

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    This file contains all the environnement variable
+    For the project
+"""
+HBNB_ENV = 'dev'
+HBNB_MYSQL_USER = 'your_mysql_username'
+HBNB_MYSQL_PWD = 'your_mysql_password'
+HBNB_MYSQL_HOST = 'localhost'
+HBNB_MYSQL_DB = 'your_database_name'
+HBNB_TYPE_STORAGE = 'db'
