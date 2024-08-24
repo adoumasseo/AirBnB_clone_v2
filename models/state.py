a0c6ec6a5cbbe01c00dbd0fbcd8091d822d29ba5
adoumasseo <adoumasseo@gmail.com>
2024-08-24 10:25:33 +0100

    [fix]: (Task 7) Add the if statement to check if Storage_type != db

--- models/state.py
+++ models/state.py
@@ -6,7 +6,10 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
 import shlex
+from os import getenv
 
+
+STORAGE_TYPE = getenv("HBNB_TYPE_STORAGE")
 
 class State(BaseModel, Base):
     """This is the class for State
@@ -18,17 +21,18 @@ class State(BaseModel, Base):
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        list_cities = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                list_cities.append(var[key])
-        for obj in list_cities:
-            if (obj.state_id == self.id):
-                result.append(obj)
-        return (result)
+    if STORAGE_TYPE != 'db':
+        @property
+        def cities(self):
+            var = models.storage.all()
+            list_cities = []
+            result = []
+            for key in var:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    list_cities.append(var[key])
+            for obj in list_cities:
+                if (obj.state_id == self.id):
+                    result.append(obj)
+            return (result)
